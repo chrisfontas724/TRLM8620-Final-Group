2c717145cab580db716d527ecdb55b592213d4d0
Chris Fontas <chrisfontas724@gmail.com>
2024-05-06 19:01:22 -0700

    fix hardcoded title

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -5,11 +5,12 @@ let Home = {
     render : async () => {
         //fetch locale-sensitive strings via i18n method
         let welcomeSubtitle = i18n.getString("Home", "welcomeSubtitle");
+        let welcomeTitle = i18n.getString("Home","welcomeTitle");
 
         //view is solely for HTML markup, contains no static text
         let view = `
                     <section class="welcome">
-                        <h1 class="center">Welcome to the Nintendo Store!</h1>
+                        <h1 class="center">${welcomeTitle}</h1>
                         <h3 class="center white">${welcomeSubtitle}</h3>
                     </section>
                     <div class="browseGrid homeGrid">`;
