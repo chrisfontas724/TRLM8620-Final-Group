5c16aa4ca1c1d3dfd3bf618ad4e53bf79dadcc80
Chris Fontas <chrisfontas724@gmail.com>
2024-05-06 19:50:02 -0700

    fix hardcoded categories

--- src/views/pages/Browse.js
+++ src/views/pages/Browse.js
@@ -12,15 +12,15 @@ let Browse = {
 
         let productMap = null;
 
-        let title = "All ";
+        let title = i18n.getString("Categories", "all");
 
         if(type == "droids") {
             productMap = productList.get('droids');
-            title += "games";
+            title +=  i18n.getString("Categories", "games");
         }
         else if(type == "vehicles") {
             productMap = productList.get('vehicles');
-            title += "hardware";
+            title +=  i18n.getString("Categories", "hardware");
         }
 
         //view is solely for HTML markup, contains no static text
